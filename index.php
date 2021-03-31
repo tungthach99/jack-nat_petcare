bc79f482d2728ce29aafabbd38391fa64f2104b4
trunghy051 <trunghy.051@gmail.com>
2021-03-31 22:35:17 +0700

    đổi index2 thành index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">   
-    <link href="css/main.css" rel="stylesheet" />
+   
     <!-- Mobile Metas -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
  
@@ -21,28 +21,34 @@
     <link rel="stylesheet" href="css/bootstrap.min.css">    
 	<!-- Site CSS -->
     <link rel="stylesheet" href="css/style.css">    
-	<link rel="stylesheet" href="css/style1.css">   
     <!-- Responsive CSS -->
     <link rel="stylesheet" href="css/responsive.css">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="css/custom.css">
-
-	<script type="text/javascript" src="js/main.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
 	<link rel="stylesheet" type="text/css" href="css/font-awesome/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="css/css3.css">
+
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
+    
+    <link href="css/boxicons/css/boxicons.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/css3.css">
+    
 </head>
 
 <body>
-	<?php
+	<!-- Start header -->
+    <?php
 	include("public/ketnoi.php");
 	include("layout/header.php");
 	include("layout/taikhoan.php");
 	?>
+	
+	<!-- End header -->
+	
 	<!-- Start slides -->
 	<div id="slides" class="cover-slides">
 		<ul class="slides-container">
@@ -53,7 +59,7 @@
 						<div class="col-md-12">
 							<h1 class="m-b-20"><strong>Welcome To <br> Jack & Nat pet care</strong></h1>
 							<p class="m-b-40">"Không biết từ bao giờ, những em cún, em mèo đã trở thành một phần của cuộc sống tôi"</p>
-							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="menu.php?&tensanpham=">Xem thêm</a></p>
+							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="menu.php">Xem thêm</a></p>
 						</div>
 					</div>
 				</div>
@@ -65,7 +71,7 @@
 						<div class="col-md-12">
 							<h1 class="m-b-20"><strong>Welcome To <br> Jack & Nat pet care</strong></h1>
 							<p class="m-b-40">"Không biết từ bao giờ, những em cún, em mèo đã trở thành một phần của cuộc sống tôi"</p>
-							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="menu.php?&tensanpham=">Xem thêm</a></p>
+							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="menu.php">Xem thêm</a></p>
 						</div>
 					</div>
 				</div>
@@ -77,7 +83,7 @@
 						<div class="col-md-12">
 							<h1 class="m-b-20"><strong>Welcome To <br> Jack & Nat pet care</strong></h1>
 							<p class="m-b-40">"Không biết từ bao giờ, những em cún, em mèo đã trở thành một phần của cuộc sống tôi"</p>
-							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="menu.php?&tensanpham=">Xem thêm</a></p>
+							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="menu.php">Xem thêm</a></p>
 						</div>
 					</div>
 				</div>
@@ -89,7 +95,40 @@
 		</div>
 	</div>
 	<!-- End slides -->
-	
+	<!-- ======= Clients Section ======= -->
+<section id="clients" class="clients">
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center" data-aos="zoom-in" data-aos-delay="100">
+        <img src="images/clients/client-1.png" class="img-fluid" alt="">
+      </div>
+
+      <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center" data-aos="zoom-in" data-aos-delay="200">
+        <img src="images/clients/client-2.png" class="img-fluid" alt="">
+      </div>
+
+      <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center" data-aos="zoom-in" data-aos-delay="300">
+        <img src="images/clients/client-3.png" class="img-fluid" alt="">
+      </div>
+
+      <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center" data-aos="zoom-in" data-aos-delay="400">
+        <img src="images/clients/client-4.png" class="img-fluid" alt="">
+      </div>
+
+      <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center" data-aos="zoom-in" data-aos-delay="500">
+        <img src="images/clients/client-5.png" class="img-fluid" alt="">
+      </div>
+
+      <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center" data-aos="zoom-in" data-aos-delay="600">
+        <img src="images/clients/client-6.png" class="img-fluid" alt="">
+      </div>
+
+    </div>
+
+  </div>
+</section><!-- End Clients Section -->
 	<!-- Start About -->
 	<div class="about-section-box">
 		<div class="container">
@@ -125,69 +164,77 @@
 		</div>
 	</div>
 	<!-- End QT -->
-	
+    
 	<!-- Start Menu -->
-	<div class="row leCacMuc" style="width: 100%; margin-top: 15px;">
-		<span class="col-sm-1"></span>
-		<span class="col-sm-10 noiDungGioiThieu">
-			<h1 style="font-size: 40px; text-align: center; font-weight: bold">SẢN PHẨM BÁN CHẠY NHẤT</h1>
-			<div style="text-align: center;">
-			
-			<div class="row" style=" width: 101%;">
-				<label id="labelTrai" for="trai"><i class="fa fa-angle-left"></i></label>
-				<label id="lablePhai" for="phai"><i class="fa fa-angle-right"></i></label>
-				<div class="slide_sp">
-				<div class="slides_sp">
-					<input type="radio" name="dieuHuong" id="trai" checked>
-					<input type="radio" name="dieuHuong" id="phai">
-					<?php
-						$sql="SELECT a.ten_san_pham, a.don_gia,a.id_san_pham,SUM(b.so_luong), a.anh
-						FROM tbl_san_pham AS a INNER JOIN tbl_chi_tiet_don_hang AS b ON a.id_san_pham = b.id_san_pham 
-						GROUP BY a.id_san_pham ORDER BY SUM(b.so_luong) DESC LIMIT 6";
-						$result=$con->query($sql);
-						if($result->num_rows>0)
-						{
-							$i=0;
-							while($row=$result->fetch_assoc())
-							{
-								$i=$i+1;
-								?>
-								<div class="thanhPhan <?php if($i==1) echo "s1"; ?>">
-									<span class="hoverSanPham">
-									<a href="menu.php?product=1&masanpham=<?php echo $row['id_san_pham']?>"><i class="fa fa-external-link" title="Mở liên kết"></i></a>
-									</span>
-									<div class="anhSanPham">
-										<img src="images/san-pham/<?php echo $row['anh']; ?>">
-									</div>
-									<div style="font-size: 16px;">
-										<?php echo $row["ten_san_pham"];?>
-									</div>
-									<div style="font-size: 16px; color: red; font-weight: bold;">
-									Giá: <?php echo number_format($row["don_gia"]) ?>"<sup><u>đ</u></sup></span>
-									<span class='giaGachNgang'></span>
-									</div>
-									<span class="giaDo"></span>
-								</div>
-					<?php
-							}
-						}
-					?>
-				</div>
-				</div>
-			</div>
-			
-			</div>
-		</span>
-	</div>
+	<!-- End Counts Section -->
+
+<!-- ======= Services Section ======= -->
+    <section id="services" class="services section-bg">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="section-title" data-aos="fade-right">
+                <img src="images/cut.png" alt="" class="img-fluid">
+                
+                <h1>DỊCH VỤ</h1>
+                <p>
+                    Jack&Nat pet care | luôn chú trọng nguồn nguyên liệu XANH – SẠCH, chất lượng hảo hạng, đảm bảo lần chăm sóc tốt nhất cho khách. 
+                    <br> <br>
+                </p>
+                Jack&Nat pet care mong muốn mang đến trải nghiệm tuyệt vời, tạo nên cuộc vui hết ý cho nhiều thực khách.
+                </p>
+            </div>
+        </div>
+        <div class="col-lg-8">
+        <div class="row">
+            <div class="col-md-6 d-flex align-items-stretch">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="100">
+                <div class="icon"><i class="bx bxl-dribbble"></i></div>
+                <h4><a href="">Uy Tín</a></h4>
+                <p>Luông là sự lựa chọn số 1 dành cho khách hàng</p>
+            </div>
+            </div>
+
+            <div class="col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="200">
+                <div class="icon"><i class="bx bx-file"></i></div>
+                <h4><a href="">Chất Lượng</a></h4>
+                <p>Chúng tôi luôn đặt sự lựa chọn sản phẩm lên hàng đầu và được chọn lọc kỹ càng.</p>
+            </div>
+            </div>
+
+            <div class="col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="300">
+                <div class="icon"><i class="bx bx-tachometer"></i></div>
+                <h4><a href="">Đội Ngũ</a></h4>
+                <p>Với đội ngũ 5 nhân viên, được lựa chọn để phục vụ khách hàng một cách chu đáo nhất</p>
+            </div>
+            </div>
+            <div class="col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box" data-aos="zoom-in" data-aos-delay="400">
+                <div class="icon"><i class="bx bx-world"></i></div>
+                <h4><a href="">Khách Hàng</a></h4>
+                <p>Đạt tỷ lệ 80% khách hàng cũ quay lại với Jack&Nat pet care</p>
+            </div>
+            </div>
+
+        </div>
+        </div>
+    </div>
+
+    </div>
+    </section><!-- End Services Section -->
 	<!-- End Menu -->
+    
 	
-	<!-- Start Gallery -->
-	<!-- <div class="gallery-box">
+	 <!-- Start Gallery -->
+	 <div class="gallery-box">
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-lg-12">
 					<div class="heading-title text-center">
-						<h2>Gallery</h2>
+						<h2>Bộ Sưu Tập</h2>
 						<p>Lorem Ipsum is simply dummy text of the printing and typesetting</p>
 					</div>
 				</div>
@@ -195,46 +242,122 @@
 			<div class="tz-gallery">
 				<div class="row">
 					<div class="col-sm-12 col-md-4 col-lg-4">
-						<a class="lightbox" href="images/gallery-img-01.jpg">
-							<img class="img-fluid" src="images/gallery-img-01.jpg" alt="Gallery Images">
+						<a class="lightbox" href="images/gallery_10.png">
+							<img class="img-fluid" src="images/gallery_10.png" alt="Gallery Images">
 						</a>
 					</div>
 					<div class="col-sm-6 col-md-4 col-lg-4">
-						<a class="lightbox" href="images/gallery-img-02.jpg">
-							<img class="img-fluid" src="images/gallery-img-02.jpg" alt="Gallery Images">
+						<a class="lightbox" href="images/gallery_02.jpeg">
+							<img class="img-fluid" src="images/gallery_02.jpeg" alt="Gallery Images">
 						</a>
 					</div>
 					<div class="col-sm-6 col-md-4 col-lg-4">
-						<a class="lightbox" href="images/gallery-img-03.jpg">
-							<img class="img-fluid" src="images/gallery-img-03.jpg" alt="Gallery Images">
+						<a class="lightbox" href="images/gallery_07.jpeg">
+							<img class="img-fluid" src="images/gallery_07.jpeg" alt="Gallery Images">
 						</a>
 					</div>
 					<div class="col-sm-12 col-md-4 col-lg-4">
-						<a class="lightbox" href="images/gallery-img-04.jpg">
-							<img class="img-fluid" src="images/gallery-img-04.jpg" alt="Gallery Images">
+						<a class="lightbox" href="images/gallery_11.jpg">
+							<img class="img-fluid" src="images/gallery_11.jpg" alt="Gallery Images">
 						</a>
 					</div>
 					<div class="col-sm-6 col-md-4 col-lg-4">
-						<a class="lightbox" href="images/gallery-img-05.jpg">
-							<img class="img-fluid" src="images/gallery-img-05.jpg" alt="Gallery Images">
-						</a>
-					</div> 
-					<div class="col-sm-6 col-md-4 col-lg-4">
-						<a class="lightbox" href="images/gallery-img-06.jpg">
-							<img class="img-fluid" src="images/gallery-img-06.jpg" alt="Gallery Images">
+						<a class="lightbox" href="images/gallery_06.jpg">
+							<img class="img-fluid" src="images/gallery_06.jpg" alt="Gallery Images">
 						</a>
 					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/gallery_09.jpg">
+							<img class="img-fluid" src="images/gallery_09.jpg" alt="Gallery Images">
+						</a>
+					</div>
+					
 				</div>
 			</div>
 		</div>
 	</div> -->
+	
+	
 	<!-- End Gallery -->
 	
+	<div class="menu-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>Jack & Nat pet care</h2>
+						<p>Thú cưng là gia đình</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="special-menu text-center">
+						<div class="button-group filter-button-group">
+							<button class="active" data-filter="*">Tất cả</button>
+							<button data-filter=".thucan">Thức ăn</button>
+							<button data-filter=".phukien">Phụ kiện</button>
+							<button data-filter=".chuongnha">Chuồng/Nhà</button>
+						</div>
+					</div>
+				</div>
+			</div>
+				
+			<div class="row special-list">
+				<div class="col-lg-4 col-md-6 special-grid thucan">
+					<div class="gallery-single fix">
+						<a href="product-detail.php">
+						<img src="images/san-pham/anh1.jpg" style="width: 290px;height: 214px;" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Thức ăn cho chó con ROYAL CANIN Bulldog Puppy</h4>
+							<p></p>
+							<h5> 230.000 VND</h5>
+						</div>
+					</a>
+					</div>
+				</div>
+				
+				<div class="col-lg-4 col-md-6 special-grid thucan">
+					<div class="gallery-single fix">
+                    <a href="product-detail.php">   
+						<img src="images/san-pham/anh2.jpg" style="width: 290px;height: 214px;" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Thức ăn cho chó MOSHM Yorkshire Grain Free Nutrition</h4>
+							<p></p>
+							<h5> 470.000 VND</h5>
+						</div>
+                        </a>
+					</div>
+				</div>
+				
+				<div class="col-lg-4 col-md-6 special-grid phukien">
+					<div class="gallery-single fix">
+                    <a href="product-detail.php"> 
+						<img src="images/san-pham/anh3.jpg" style="width: 290px;height: 214px;" class="img-fluid" alt="Image">
+						<div class="why-text">
+							<h4>Dây dắt cho chó mèo tự động DELE 007G</h4>
+							<h5> 465.000 VND</h5>
+						</div>
+                    </a>
+					</div>
+				</div>
+				
+				
+				
+				
+				
+			</div>
+		</div>
+	</div>
+	
 	<?php
-	include("layout/fromAdmin.php");
-	include("layout/footer.php");
+	include_once 'layout/fromAdmin.php';
+    include_once 'layout/footer.php';
+    
 	?>
 	
+	<a href="#" id="back-to-top" title="Back to top" style="display: none;">&uarr;</a>
+
 	<!-- ALL JS FILES -->
 	<script src="js/jquery-3.2.1.min.js"></script>
 	<script src="js/popper.min.js"></script>
@@ -247,10 +370,7 @@
 	<script src="js/form-validator.min.js"></script>
     <script src="js/contact-form-script.js"></script>
     <script src="js/custom.js"></script>
-	
-	<?php
-	include("layout/vedautrang.php")
-	?>
-	
+    <?php include_once 'layout/vedautrang.php'; ?>
+    <script src="css/aos/aos.js"></script>
 </body>
 </html>
---
+++ index3.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en"><!-- Basic -->
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
+    <link href="css/main.css" rel="stylesheet" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+     <!-- Site Metas -->
+    <title>Jack & Nat pet care</title>  
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="images/icon-logo.png" type="image/x-icon">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">    
+	<!-- Site CSS -->
+    <link rel="stylesheet" href="css/style.css">    
+	<link rel="stylesheet" href="css/style1.css">   
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
+
+	<script type="text/javascript" src="js/main.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/css3.css">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+	<?php
+	include("public/ketnoi.php");
+	include("layout/header.php");
+	include("layout/taikhoan.php");
+	?>
+	<!-- Start slides -->
+	<div id="slides" class="cover-slides">
+		<ul class="slides-container">
+			<li class="text-center">
+				<img src="images/dog.jfif" alt="">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<h1 class="m-b-20"><strong>Welcome To <br> Jack & Nat pet care</strong></h1>
+							<p class="m-b-40">"Không biết từ bao giờ, những em cún, em mèo đã trở thành một phần của cuộc sống tôi"</p>
+							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="menu.php?&tensanpham=">Xem thêm</a></p>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="text-center">
+				<img src="images/photod.jfif" alt="">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<h1 class="m-b-20"><strong>Welcome To <br> Jack & Nat pet care</strong></h1>
+							<p class="m-b-40">"Không biết từ bao giờ, những em cún, em mèo đã trở thành một phần của cuộc sống tôi"</p>
+							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="menu.php?&tensanpham=">Xem thêm</a></p>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="text-center">
+				<img src="images/four.jfif" alt="">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<h1 class="m-b-20"><strong>Welcome To <br> Jack & Nat pet care</strong></h1>
+							<p class="m-b-40">"Không biết từ bao giờ, những em cún, em mèo đã trở thành một phần của cuộc sống tôi"</p>
+							<p><a class="btn btn-lg btn-circle btn-outline-new-white" href="menu.php?&tensanpham=">Xem thêm</a></p>
+						</div>
+					</div>
+				</div>
+			</li>
+		</ul>
+		<div class="slides-navigation">
+			<a href="#" class="next"><i class="fa fa-angle-right" aria-hidden="true"></i></a>
+			<a href="#" class="prev"><i class="fa fa-angle-left" aria-hidden="true"></i></a>
+		</div>
+	</div>
+	<!-- End slides -->
+	
+	<!-- Start About -->
+	<div class="about-section-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-sm-12">
+					<img src="images/8299380e569463c0c51f18f47d2d86da.jpg" alt="" class="img-fluid">
+				</div>
+				<div class="col-lg-6 col-md-6 col-sm-12 text-center">
+					<div class="inner-column">
+						<h1>Welcome To <span>Jack & Nat Pet Care</span></h1>
+						<h4>Câu chuyện nhỏ</h4>
+						<p>Mỗi con vật đến với cửa hàng thú cưng của chúng tôi đều được gọi là em, vì chúng được nâng niu và chăm sóc cẩn thận như các em bé vậy. Các em đến đây được chăm sóc theo quy trình từ vệ sinh tai, vắt tuyến hôi cho đến tắm rửa, sấy khô.</p>
+						<p>Nhiều người nói chó, mèo cần gì chăm sóc kỹ thế. Nhưng tôi không nghĩ vậy. Thú cưng từ bao lâu nay đã trở thành như các thành viên trong mỗi gia đình. Chúng ta con người được quan tâm, chăm sóc và cả làm đẹp thì các em cún, em mèo cũng không nên ngoại lệ.</p>
+						<a class="btn btn-lg btn-circle btn-outline-new-white" href="about.php">Xem thêm</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End About -->
+	
+	<!-- Start QT -->
+	<div class="qt-box qt-background">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 ml-auto mr-auto text-left">
+					<p class="lead ">
+						" Một chú mèo lạc giữa loài động vật giống như nụ hồng nhỏ nhắn nở rộ giữa một bụi gai. "
+					</p>
+					<span class="lead">Khuyết danh</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End QT -->
+	
+	<!-- Start Menu -->
+	<div class="row leCacMuc" style="width: 100%; margin-top: 15px;">
+		<span class="col-sm-1"></span>
+		<span class="col-sm-10 noiDungGioiThieu">
+			<h1 style="font-size: 40px; text-align: center; font-weight: bold">SẢN PHẨM BÁN CHẠY NHẤT</h1>
+			<div style="text-align: center;">
+			
+			<div class="row" style=" width: 101%;">
+				<label id="labelTrai" for="trai"><i class="fa fa-angle-left"></i></label>
+				<label id="lablePhai" for="phai"><i class="fa fa-angle-right"></i></label>
+				<div class="slide_sp">
+				<div class="slides_sp">
+					<input type="radio" name="dieuHuong" id="trai" checked>
+					<input type="radio" name="dieuHuong" id="phai">
+					<?php
+						$sql="SELECT a.ten_san_pham, a.don_gia,a.id_san_pham,SUM(b.so_luong), a.anh
+						FROM tbl_san_pham AS a INNER JOIN tbl_chi_tiet_don_hang AS b ON a.id_san_pham = b.id_san_pham 
+						GROUP BY a.id_san_pham ORDER BY SUM(b.so_luong) DESC LIMIT 6";
+						$result=$con->query($sql);
+						if($result->num_rows>0)
+						{
+							$i=0;
+							while($row=$result->fetch_assoc())
+							{
+								$i=$i+1;
+								?>
+								<div class="thanhPhan <?php if($i==1) echo "s1"; ?>">
+									<span class="hoverSanPham">
+									<a href="menu.php?product=1&masanpham=<?php echo $row['id_san_pham']?>"><i class="fa fa-external-link" title="Mở liên kết"></i></a>
+									</span>
+									<div class="anhSanPham">
+										<img src="images/san-pham/<?php echo $row['anh']; ?>">
+									</div>
+									<div style="font-size: 16px;">
+										<?php echo $row["ten_san_pham"];?>
+									</div>
+									<div style="font-size: 16px; color: red; font-weight: bold;">
+									Giá: <?php echo number_format($row["don_gia"]) ?>"<sup><u>đ</u></sup></span>
+									<span class='giaGachNgang'></span>
+									</div>
+									<span class="giaDo"></span>
+								</div>
+					<?php
+							}
+						}
+					?>
+				</div>
+				</div>
+			</div>
+			
+			</div>
+		</span>
+	</div>
+	<!-- End Menu -->
+	
+	<!-- Start Gallery -->
+	<!-- <div class="gallery-box">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>Gallery</h2>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+					</div>
+				</div>
+			</div>
+			<div class="tz-gallery">
+				<div class="row">
+					<div class="col-sm-12 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/gallery-img-01.jpg">
+							<img class="img-fluid" src="images/gallery-img-01.jpg" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/gallery-img-02.jpg">
+							<img class="img-fluid" src="images/gallery-img-02.jpg" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/gallery-img-03.jpg">
+							<img class="img-fluid" src="images/gallery-img-03.jpg" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-12 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/gallery-img-04.jpg">
+							<img class="img-fluid" src="images/gallery-img-04.jpg" alt="Gallery Images">
+						</a>
+					</div>
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/gallery-img-05.jpg">
+							<img class="img-fluid" src="images/gallery-img-05.jpg" alt="Gallery Images">
+						</a>
+					</div> 
+					<div class="col-sm-6 col-md-4 col-lg-4">
+						<a class="lightbox" href="images/gallery-img-06.jpg">
+							<img class="img-fluid" src="images/gallery-img-06.jpg" alt="Gallery Images">
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div> -->
+	<!-- End Gallery -->
+	
+	<?php
+	include("layout/fromAdmin.php");
+	include("layout/footer.php");
+	?>
+	
+	<!-- ALL JS FILES -->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+	<script src="js/jquery.superslides.min.js"></script>
+	<script src="js/images-loded.min.js"></script>
+	<script src="js/isotope.min.js"></script>
+	<script src="js/baguetteBox.min.js"></script>
+	<script src="js/form-validator.min.js"></script>
+    <script src="js/contact-form-script.js"></script>
+    <script src="js/custom.js"></script>
+	
+	<?php
+	include("layout/vedautrang.php")
+	?>
+	
+</body>
+</html>
