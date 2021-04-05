e6e06b9c3e024ca1e41f36e04d437914a1848d6a
tungthach99 <73577391+tungthach99@users.noreply.github.com>
2021-04-05 15:41:49 +0700

    ko quan trọng

--- customer/product/product_info_2.php
+++ customer/product/product_info_2.php
@@ -15,80 +15,70 @@
 	<!-- Start Menu -->
 	<div class="menu-box">
 		<div class="container">
-<!--
-			<div class="row">
-				<div class="col-lg-12">
-					<div class="heading-title text-center">
-						<h2>Jack & Nat pet care</h2>
-						<p>Thú cưng là gia đình</p>
-					</div>
-				</div>
-			</div>
--->
             <div class="row" style="margin-top: 94px;">
 				<div class="col-lg-12">
 					<div class="special-menu text-center">
 						<div class="button-group filter-button-group">
-							<button class="active" data-filter="*">Tất cả</button>
-							<button data-filter=".thuy">Thú y</button>
-							<button data-filter=".trongcoi">Trông coi</button>
-							<button data-filter=".thammy">Thẩm mỹ</button>
+							<button <?php if(!isset($_GET['a'])) echo "class='active'"?> data-filter="*">Tất cả</button>
+							<button <?php if(isset($_GET['a']) and $_GET['a'] == "Thú y") echo "class='active'"?> data-filter=".thuy">Thú y</button>
+							<button <?php if(isset($_GET['a']) and $_GET['a'] == "Trông coi") echo "class='active'"?> data-filter=".trongcoi">Trông coi</button>
+							<button <?php if(isset($_GET['a']) and $_GET['a'] == "Thẩm mỹ") echo "class='active'"?> data-filter=".thammy">Thẩm mỹ</button>
 						</div>
 					</div>
 				</div>
 			</div>
 			<div class="row special-list">
 			<?php
-					$sql="SELECT *,
+					$sql="SELECT *
 								FROM tbl_dich_vu ";
                     $result=$con->query($sql);
                     if($result->num_rows>0)
 						{
 							while($row=$result->fetch_assoc())
 							{ //for->while
-							if($row['id_danh_muc']=='11')
+							if($row['id_danh_muc'] == '4')
 							{
 					?>
 							<div class="col-lg-4 col-md-6 special-grid thuy">
 								<div class="gallery-single fix">
-									<img src="images/san-pham/<?php echo $row['anh'] ?>" alt="" style="width: 290px;height: 214px" class="img-fluid" alt="Image">
+									<img src="images/san-pham/<?php echo $row['anh_dv'] ?>" alt="" style="width: 290px;height: 214px" class="img-fluid" alt="Image">
 									<div class="why-text">
-										<h4><?php  echo $row['ten_san_pham'] ?></h4>
+										<h4><?php  echo $row['ten_dich_vu']; ?></h4>
 										<p></p>
-										<h5><?php echo number_format($row['don_gia']) ?>  VND</h5>
-										<a href="menu.php?product=1&masanpham=<?php echo $row['id_san_pham']?>"><button class="nutChiTiet">Chi tiết</button></a>
+										<h5><?php echo number_format($row['don_gia_dv']) ?>  VND</h5>
+										<a href="dichvu.php?&m=<?php echo $row['id_dich_vu'] ?>"><button class="nutChiTiet">Chi tiết</button></a>
 									</div>
 								</div>
 							</div>
 							<?php
 								}
-								if($row['id_danh_muc']=='12')
+								if($row['id_danh_muc']=='5')
 								{
 							?>
 							<div class="col-lg-4 col-md-6 special-grid trongcoi">
 								<div class="gallery-single fix">
-									<img src="images/san-pham/<?php echo $row['anh'] ?>" alt="" style="width: 290px;height: 214px;" class="img-fluid" alt="Image">
+									<img src="images/san-pham/<?php echo $row['anh_dv'] ?>" alt="" style="width: 290px;height: 214px" class="img-fluid" alt="Image">
 									<div class="why-text">
-										<h4><?php echo $row['ten_san_pham'] ?></h4>
+										<h4><?php  echo $row['ten_dich_vu']; ?></h4>
 										<p></p>
-										<h5><?php echo number_format($row['don_gia']) ?>  VND</h5>
-										<a href="menu.php?product=1&masanpham=<?php echo $row['id_san_pham']?>"><button class="nutChiTiet">Chi tiết</button></a>
+										<h5><?php echo number_format($row['don_gia_dv']) ?>  VND</h5>
+										<a href="dichvu.php?&m=<?php echo $row['id_dich_vu'] ?>"><button class="nutChiTiet">Chi tiết</button></a>
 									</div>
 								</div>
 							</div>
 							<?php
 								}
-								if($row['id_danh_muc']=='13')
+								if($row['id_danh_muc']=='6')
 								{
 							?>
 							<div class="col-lg-4 col-md-6 special-grid thammy">
 								<div class="gallery-single fix">
-									<img src="images/san-pham/<?php echo $row['anh'] ?>" alt="" style=" width: 290px;height: 214px;" class="img-fluid" alt="Image">
+									<img src="images/san-pham/<?php echo $row['anh_dv'] ?>" alt="" style="width: 290px;height: 214px" class="img-fluid" alt="Image">
 									<div class="why-text">
-										<h4><?php echo $row['ten_san_pham'] ?></h4>
+										<h4><?php  echo $row['ten_dich_vu']; ?></h4>
 										<p></p>
-										<h5><?php echo number_format($row['don_gia']) ?>  VND</h5>
-										<a href="menu.php?product=1&masanpham=<?php echo $row['id_san_pham']?>"><button class="nutChiTiet">Chi tiết</button></a>
+										<h5><?php echo number_format($row['don_gia_dv']) ?>  VND</h5>
+										<a href="dichvu.php?&m=<?php echo $row['id_dich_vu'] ?>"><button class="nutChiTiet">Chi tiết</button></a>
 									</div>
 								</div>
 							</div>
